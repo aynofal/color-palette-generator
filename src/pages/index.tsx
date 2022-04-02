c5565997357cd091eca211f6a773eebfcbae830a
Ammar Nofal <ammar.nofal@raisin.com>
2022-04-03 00:15:22 +0200

    Created color generator.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import * as React from "react"
+import {RGBColor, SketchPicker} from 'react-color'
+import {useCallback, useEffect, useMemo, useState} from "react";
 
 // styles
 const pageStyles = {
@@ -6,172 +8,87 @@ const pageStyles = {
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+
+function componentToHex(c: number): string {
+  const hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
+function rgbToHex({r, g, b}: RGBColor): string {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
+function getColorWithAlpha(color: number, background: number, alpha: number): number {
+  return color * alpha + background * (1 - alpha)
 }
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "TypeScript Documentation",
-  url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
 
 // markup
 const IndexPage = () => {
+  const [mainColor, setMainColor] = useState<RGBColor>({r: 0, g: 0, b: 0});
+  const [bgColor, setBgColor] = useState<RGBColor>({r: 255, g: 255, b: 255});
+  const [colorName, setColorName] = useState<string>('white');
+  const formatColorWithAlpha: (alpha: number) => RGBColor = useCallback<(alpha: number) => RGBColor>((alpha) => ({
+    r: Math.round(getColorWithAlpha(mainColor.r, bgColor.r, alpha)),
+    g: Math.round(getColorWithAlpha(mainColor.g, bgColor.g, alpha)),
+    b: Math.round(getColorWithAlpha(mainColor.b, bgColor.b, alpha)),
+  }), [mainColor, bgColor])
+  useEffect(() => {
+    console.log(mainColor,bgColor)
+  }, [mainColor, bgColor])
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        🎉🎉🎉
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <title>Color generation tool</title>
+      <div>
+        <h2>Color Pickers</h2>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <div>
+            <h4>Main Color</h4>
+            <SketchPicker onChange={(colorResult) => setMainColor(colorResult.rgb)} color={mainColor}/>
+          </div>
+          <div style={{width: '20px'}}/>
+          <div>
+            <h4>Background Color</h4>
+            <SketchPicker onChange={(colorResult) => setBgColor(colorResult.rgb)} color={bgColor}/>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>Color Name</h2>
+        <input type={'text'} value={colorName} onChange={(e) => {
+          e.preventDefault();
+          setColorName(e.target.value);
+        }}/>
+        <h2>Values</h2>
+        <table style={{border: '1px solid', borderCollapse: 'collapse'}}>
+          <tr style={{border: '1px solid', borderCollapse: 'collapse'}}>
+            <th style={{border: '1px solid', borderCollapse: 'collapse', padding: '5px'}}>
+              Color
+            </th>
+            <th style={{border: '1px solid', borderCollapse: 'collapse', padding: '5px'}}>
+              Name
+            </th>
+            <th style={{border: '1px solid', borderCollapse: 'collapse', padding: '5px'}}>
+              Hex
+            </th>
+          </tr>
+          {
+            [...Array(100).keys()].reverse().map(value => {
+              const alpha = useMemo(() => (value + 1)/100, [mainColor, bgColor]);
+              const color = useMemo(() => rgbToHex(formatColorWithAlpha(alpha)), [mainColor, bgColor, alpha])
+              return (<tr>
+                <td style={{border: '1px solid', borderCollapse: 'collapse', padding: '5px', backgroundColor: color}}/>
+                <td style={{border: '1px solid', borderCollapse: 'collapse', padding: '5px'}}>
+                  {`${colorName}_${value+1}`}
+                </td>
+                <td style={{border: '1px solid', borderCollapse: 'collapse', padding: '5px'}}>
+                  {color}
+                </td>
+              </tr>)
+            })
+          }
+        </table>
+
+      </div>
     </main>
   )
 }
